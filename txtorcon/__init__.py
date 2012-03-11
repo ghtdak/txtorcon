1bc9e060c264666c7a7e5c0426db0db8585282cf
meejah <meejah@meejah.ca>
2012-03-11 10:21:06 +0000

    first working cut at real hidden service endpoint [yapf]

--- txtorcon/__init__.py
+++ txtorcon/__init__.py
@@ -10,7 +10,7 @@ from circuit import Circuit
 from stream import Stream
 from torcontrolprotocol import TorControlProtocol, TorProtocolFactory, DEFAULT_VALUE
 from torstate import TorState, build_tor_connection
-from torconfig import TorConfig, HiddenService, TorProcessProtocol, launch_tor
+from torconfig import TorConfig, HiddenService, TorProcessProtocol, TCP4HiddenServiceEndpoint, launch_tor
 from addrmap import AddrMap
 from addrmap import Addr
 import util
@@ -19,7 +19,7 @@ from interface import *
 
 __all__ = ["Router", "Circuit", "Stream", "TorControlProtocol", "TorState",
            "DEFAULT_VALUE", "build_tor_connection", "launch_tor", "TorConfig",
-           "HiddenService", "TorProcessProtocol", "AddrMap", "util",
-           "interface", "ITorControlprotocol", "IStreamListener",
-           "IStreamAttacher", "ICircuitContainer", "ICircuitListener",
-           "IRouterContainer"]
+           "HiddenService", "TorProcessProtocol", "TCP4HiddenServiceEndpoint",
+           "AddrMap", "util", "interface", "ITorControlprotocol",
+           "IStreamListener", "IStreamAttacher", "ICircuitContainer",
+           "ICircuitListener", "IRouterContainer"]
