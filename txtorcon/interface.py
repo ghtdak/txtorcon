c950ece2e36bbf7c77175450981ca6091974d7bb
meejah <meejah@meejah.ca>
2012-03-27 08:10:42 +0000

    add unique_routers to IRouterContainer, fix test [yapf]

--- txtorcon/interface.py
+++ txtorcon/interface.py
@@ -1,4 +1,4 @@
-from zope.interface import implements, Interface
+from zope.interface import implements, Interface, Attribute
 
 
 class IStreamListener(Interface):
@@ -178,6 +178,9 @@ class ITorControlProtocol(Interface):
 
 
 class IRouterContainer(Interface):
+
+    unique_routers = Attribute(
+        """unique_routers contains a list of all the Router instances""")
 
     def router_from_id(self, routerid):
         """
