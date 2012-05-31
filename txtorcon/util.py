ffbf0d8ef4ff3a16bf2ec642974fb2333f61e492
Michele Orrù <maker.py@gmail.com>
2012-05-31 19:32:12 +0000

    Improving find_keyword; removing trailing whitespaces in util module. [yapf]

--- txtorcon/util.py
+++ txtorcon/util.py
@@ -56,17 +56,11 @@ def find_keywords(args):
     """
     This splits up strings like name=value, foo=bar into a dict. Does NOT deal
     with quotes in value (e.g. key="value with space" will not work
-    
+
     :return:
         a dict of key->value (both strings) of all name=value type keywords found in args.
     """
-
-    kw = {}
-    for x in args:
-        if '=' in x:
-            (k, v) = x.split('=', 1)
-            kw[k] = v
-    return kw
+    return dict(x.split('=', 1) for x in args if '=' in x)
 
 
 def delete_file_or_tree(*args):
@@ -97,7 +91,7 @@ def process_from_address(addr, port, torstate=None):
     returned.
 
     If psutil isn't installed, the PIDs are returned instead of
-    psutil.Process instances.    
+    psutil.Process instances.
     """
 
     if addr == None:
@@ -118,9 +112,9 @@ def process_from_address(addr, port, torstate=None):
 
     return None
 
-    ##
-    ## classes
-    ##
+##
+## classes
+##
 
 
 class NetLocation:
