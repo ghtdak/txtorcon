0164b08e272da2736fd7f53aba64cec83b1facbd
meejah <meejah@meejah.ca>
2012-06-06 00:42:28 +0000

    cleanup distribution more [yapf]
    include everything we actually want in MANIFEST.in, get rid of pre-built documentation, make pypi uploading target

--- setup.py
+++ setup.py
@@ -12,13 +12,16 @@ setup(
     'Twisted-based Tor controller client, with state-tracking and configuration abstractions.',
     long_description=open('README', 'r').read(),
     keywords=['python', 'twisted', 'tor', 'tor controller'],
-    requires=['twisted (>10.1.0)', 'GeoIP', 'ipaddr'],
-    classifiers=
-    ['Framework :: Twisted', 'License :: OSI Approved :: MIT License',
-     'Natural Language :: English', 'Operating System :: POSIX :: Linux',
-     'Operating System :: Unix', 'Programming Language :: Python',
-     'Topic :: Internet :: Proxy Servers', 'Topic :: Internet',
-     'Topic :: Security'],
+    requires=['twisted (>11.1.0)', 'pygeoip', 'psutil', 'ipaddr'],
+    classifiers=['Framework :: Twisted', 'Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: Unix', 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Topic :: Internet :: Proxy Servers', 'Topic :: Internet',
+                 'Topic :: Security'],
     author=__author__,
     author_email=__contact__,
     url=__url__,
@@ -28,24 +31,18 @@ setup(
     ## I'm a little unclear if I'm doing this "properly", especially
     ## the documentation etc. Do we really want "share/txtorcon" for
     ## the first member of the tuple?
-    data_files
-    =[('share/txtorcon', ['INSTALL', 'README', 'TODO', 'meejah.asc']),
-      ## this includes pre-built single-page HTML docs
-      ## into the distribution. the map construct grabs
-      ## everything in doc_html/_static
-      ('share/txtorcon', ['doc_html/index.html', 'doc_html/objects.inv'] + map(
-          lambda x: os.path.join('doc_html/_static', x), os.listdir(
-              'doc_html/_static'))),
-      ## this includes the Sphinx source for the
-      ## docs. The "map+filter" construct grabs all .rst
-      ## files and re-maps the path
-      ('share/txtorcon', ['docs/apilinks_sphinxext.py', 'docs/conf.py',
-                          'docs/Makefile', 'docs/avatar.png'] +
-       map(lambda x: os.path.join('docs', x), filter(
-           lambda x: x[-3:] == 'rst', os.listdir('docs'))) + map(
-               lambda x: os.path.join('docs/_static', x), os.listdir(
-                   'docs/_static'))),
-      ## include all the examples
-      ('share/txtorcon/examples', map(lambda x: os.path.join('examples', x),
-                                      filter(lambda x: x[-3:] == '.py',
-                                             os.listdir('examples'))))])
+    data_files=[('share/txtorcon', ['INSTALL', 'README', 'TODO', 'meejah.asc']),
+                ## this includes the Sphinx source for the
+                ## docs. The "map+filter" construct grabs all .rst
+                ## files and re-maps the path
+                ('share/txtorcon',
+                 ['docs/apilinks_sphinxext.py', 'docs/conf.py', 'docs/Makefile',
+                  'docs/avatar.png'] +
+                 map(lambda x: os.path.join('docs', x), filter(
+                     lambda x: x[-3:] == 'rst', os.listdir('docs'))) + map(
+                         lambda x: os.path.join('docs/_static', x), os.listdir(
+                             'docs/_static'))),
+                ## include all the examples
+                ('share/txtorcon/examples', map(
+                    lambda x: os.path.join('examples', x), filter(
+                        lambda x: x[-3:] == '.py', os.listdir('examples'))))])
