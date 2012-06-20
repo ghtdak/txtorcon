cea11dbafaa31424829efda9289f856a3cd9c8bd
meejah <meejah@meejah.ca>
2012-06-20 23:15:29 +0000

    remove psutil as semi-optional dependency from util [yapf]

--- txtorcon/test/test_util.py
+++ txtorcon/test/test_util.py
@@ -42,7 +42,7 @@ class TestProcessFromUtil(unittest.TestCase):
         pfa = process_from_address('(Tor_internal)', 80, self.fakestate)
         # depends on whether you have psutil installed or not, and on
         # whether your system always has a PID 0 process...
-        self.assertTrue(pfa.pid == self.fakestate.tor_pid)
+        self.assertTrue(pfa == self.fakestate.tor_pid)
 
     @defer.inlineCallbacks
     def test_real_addr(self):
@@ -60,7 +60,7 @@ class TestProcessFromUtil(unittest.TestCase):
         finally:
             listener.stopListening()
 
-        self.assertTrue(pid.pid == os.getpid())
+        self.assertTrue(pid == os.getpid())
 
 
 class TestDelete(unittest.TestCase):
