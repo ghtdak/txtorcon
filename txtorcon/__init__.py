dab09d8b8aa9b44be2614a16a4c49946b671e20a
Lukas Lueg <lukas.lueg@gmail.com>
2013-01-04 18:24:56 +0000

    Refactored build_tor_connection, added build_local_tor_connection [yapf]

--- txtorcon/__init__.py
+++ txtorcon/__init__.py
@@ -11,7 +11,7 @@ from txtorcon.router import Router
 from txtorcon.circuit import Circuit
 from txtorcon.stream import Stream
 from txtorcon.torcontrolprotocol import TorControlProtocol, TorProtocolError, TorProtocolFactory, DEFAULT_VALUE
-from txtorcon.torstate import TorState, build_tor_connection
+from txtorcon.torstate import TorState, build_tor_connection, build_local_tor_connection
 from txtorcon.torconfig import TorConfig, HiddenService, TorProcessProtocol, TCPHiddenServiceEndpoint, launch_tor
 from txtorcon.torinfo import TorInfo
 from txtorcon.addrmap import AddrMap
@@ -21,9 +21,10 @@ from txtorcon.interface import *
 
 __all__ = ["Router", "Circuit", "Stream", "TorControlProtocol",
            "TorProtocolError", "TorProtocolFactory", "TorState",
-           "DEFAULT_VALUE", "TorInfo", "build_tor_connection", "launch_tor",
-           "TorConfig", "HiddenService", "TorProcessProtocol", "TorInfo",
+           "DEFAULT_VALUE", "TorInfo", "build_tor_connection",
+           "build_local_tor_connection", "launch_tor", "TorConfig",
+           "HiddenService", "TorProcessProtocol", "TorInfo",
            "TCPHiddenServiceEndpoint", "AddrMap", "util", "interface",
-           "ITorControlprotocol", "IStreamListener", "IStreamAttacher",
+           "ITorControlProtocol", "IStreamListener", "IStreamAttacher",
            "StreamListenerMixin", "ICircuitContainer", "ICircuitListener",
            "CircuitListenerMixin", "IRouterContainer", "IAddrListener"]
