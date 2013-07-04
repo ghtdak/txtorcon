6990e9cfa15d1661e5986449d8758afa6f0c2290
meejah <meejah@meejah.ca>
2013-07-04 21:48:15 +0000

    Add a test for when there is no Tor binary [yapf]

--- txtorcon/__init__.py
+++ txtorcon/__init__.py
@@ -12,7 +12,7 @@ from txtorcon.circuit import Circuit
 from txtorcon.stream import Stream
 from txtorcon.torcontrolprotocol import TorControlProtocol, TorProtocolError, TorProtocolFactory, DEFAULT_VALUE
 from txtorcon.torstate import TorState, build_tor_connection, build_local_tor_connection
-from txtorcon.torconfig import TorConfig, HiddenService, TorProcessProtocol, TCPHiddenServiceEndpoint, launch_tor
+from txtorcon.torconfig import TorConfig, HiddenService, TorProcessProtocol, TCPHiddenServiceEndpoint, launch_tor, TorNotFound
 from txtorcon.torinfo import TorInfo
 from txtorcon.addrmap import AddrMap
 import util
@@ -22,8 +22,8 @@ from txtorcon.interface import *
 __all__ = ["Router", "Circuit", "Stream", "TorControlProtocol",
            "TorProtocolError", "TorProtocolFactory", "TorState",
            "DEFAULT_VALUE", "TorInfo", "build_tor_connection",
-           "build_local_tor_connection", "launch_tor", "TorConfig",
-           "HiddenService", "TorProcessProtocol", "TorInfo",
+           "build_local_tor_connection", "launch_tor", "TorNotFound",
+           "TorConfig", "HiddenService", "TorProcessProtocol", "TorInfo",
            "TCPHiddenServiceEndpoint", "AddrMap", "util", "interface",
            "ITorControlProtocol", "IStreamListener", "IStreamAttacher",
            "StreamListenerMixin", "ICircuitContainer", "ICircuitListener",
