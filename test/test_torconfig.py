ddc3724a1f05295832720facf1d42ecba7cb4c03
meejah <meejah@meejah.ca>
2014-02-15 11:46:11 +0000

    test some rare cases in TorConfig and eliminate some unused code [yapf]

--- test/test_torconfig.py
+++ test/test_torconfig.py
@@ -12,6 +12,7 @@ from twisted.python.failure import Failure
 from twisted.internet.interfaces import IReactorCore, IProtocolFactory, IReactorTCP
 
 from txtorcon import TorControlProtocol, ITorControlProtocol, TorConfig, DEFAULT_VALUE, HiddenService, launch_tor, TCPHiddenServiceEndpoint, TorNotFound
+from txtorcon import torconfig
 
 from txtorcon.util import delete_file_or_tree
 
@@ -98,7 +99,8 @@ class ConfigTests(unittest.TestCase):
     def test_boolean_parse_error(self):
         self.protocol.answers.append('config/names=\nfoo Boolean\nOK')
         self.protocol.answers.append({'foo': 'bar'})
-        TorConfig(self.protocol)
+        cfg = TorConfig(self.protocol)
+        self.assertEqual(cfg.get_type('foo'), torconfig.Boolean)
         errs = self.flushLoggedErrors(ValueError)
         self.assertEqual(len(errs), 1)
         ## dunno if asserting strings in messages is a good idea...
@@ -517,6 +519,7 @@ OK''')
         conf.hiddenservices[0].ports.append('443 127.0.0.1:443')
         self.assertTrue(conf.needs_save())
         conf.save()
+        self.assertEqual(conf.get_type('HiddenServices'), HiddenService)
 
         self.assertEqual(len(self.protocol.sets), 7)
         self.assertEqual(self.protocol.sets[0],
